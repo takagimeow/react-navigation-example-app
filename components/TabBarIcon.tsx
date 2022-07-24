76d1f7e33108b79f4c905158880a7db37d18b541
Keisuke Takagi <mit@takagi.app>
2022-07-25 03:21:51 +0900

    feat: TabBarIconコンポーネントを実装した

---
+++ components/TabBarIcon.tsx
@@ -0,0 +1,9 @@
+
+import { FontAwesome } from "@expo/vector-icons";
+
+export function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+}
